df4aa324692a04407d2a46f26ef7c069ad16a90d
jazar01 <jim.azar@orasi.com>
2017-09-04 16:49:51 -0400

    first fully functional version

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualBasic.Devices;
 
 /// <summary>
-/// allocates as much memory as possible and writes random bytes to it.
-/// under development do not use at this time.  it may lockup windows
+/// allocates as much memory as possible and writes either zeros, ones, or random bytes to it
 /// </summary>
 
 namespace Gobble
@@ -12,6 +11,8 @@ namespace Gobble
     class Program
     {
         static Random r = new Random();
+        static string option;
+        static string filler;
         struct bytes
         {
             public byte[] sequence;
@@ -21,111 +22,78 @@ namespace Gobble
 
         static void Main(string[] args)
         {
+            filler = "0's";
+            if (args.Length == 0)
+                option = "c";
+            else
+                switch (args[0])
+                {
+                    case "-r":
+                        option = "r";
+                        filler = "random bytes";
+                        break;
+                    case "-f":
+                    case "-1":
+                        option = "f";
+                        filler = "1's";
+                        break;
+                    default:
+                        option = "c";
+                        break;
+                }
+
+
             ComputerInfo CI = new ComputerInfo();
             ulong totalMem = CI.TotalPhysicalMemory;
+            ulong availablePMem = CI.AvailablePhysicalMemory;
+
 
             int MB = 1024 * 1024;
             int GB = MB * 1024;
             int TotalGBs = (int)((uint)totalMem / GB);
 
-            int GBs = 29;
+            int GBs = (int) (availablePMem / (ulong) GB);
             int size = TotalGBs;
-            int n = MB;
+
             DateTime tstart = DateTime.Now;
             arrayofbytes = new bytes[GBs];
             for (int i = 0; i < GBs; i++)
                 arrayofbytes[i].sequence = new byte[GB];
-     
-            /*
-            byte[] barray1 = new byte[GB];
-            byte[] barray2 = new byte[GB];
-            byte[] barray3 = new byte[GB];
-            byte[] barray4 = new byte[GB];
-            byte[] barray5 = new byte[GB];
-            byte[] barray6 = new byte[GB];
-            byte[] barray7 = new byte[GB];
-            byte[] barray8 = new byte[GB];
-            byte[] barray9 = new byte[GB];
-            byte[] barray10 = new byte[GB];
-            byte[] barray11 = new byte[GB];
-            byte[] barray12 = new byte[GB];
-            byte[] barray13 = new byte[GB];
-            byte[] barray14 = new byte[GB];
-            byte[] barray15 = new byte[GB];
-            byte[] barray16 = new byte[GB];
-            */
 
+            //todo allocate last bytes of memory here
+            ulong remaining = availablePMem - ((ulong) GBs * (ulong) GB );
+            byte[] lastbytes = new byte[remaining] ;
 
-
-            //for (int i = 0; i < size; i++)
-            //           Parallel.For(0, size, i =>
-            //            { bufptr[i] = new byte[n]; });
-
-
-
-            Console.WriteLine("Mem committed");
-            /*
-            r.NextBytes(barray1);
-            r.NextBytes(barray2);
-            r.NextBytes(barray3);
-            r.NextBytes(barray4);
-            r.NextBytes(barray5);
-            r.NextBytes(barray6);
-            r.NextBytes(barray7);
-            r.NextBytes(barray8);
-            r.NextBytes(barray9);
-            r.NextBytes(barray10);
-            r.NextBytes(barray11);
-            r.NextBytes(barray12);
-            r.NextBytes(barray13);
-            r.NextBytes(barray14);
-            r.NextBytes(barray15);
-            r.NextBytes(barray16);
-
-            */
-
-            
-            Parallel.For(0, GBs, new ParallelOptions { MaxDegreeOfParallelism = 4 }, i =>
+            string totalbytes = ((double)GBs + (double)lastbytes.Length / (double)GB).ToString("0.000");
+            Console.WriteLine( totalbytes + " GB committed");
+    
+            Parallel.For(0, GBs, new ParallelOptions { MaxDegreeOfParallelism = 16 }, i =>
              {
-                 //arrayofbytes[i].sequence = new byte[GB];
-                 //r.NextBytes(arrayofbytes[i].sequence);
-                 for (int r = 0; r < GB; r++)
-                     arrayofbytes[i].sequence[r] = 255;
-                 // Array.Clear(arrayofbytes[i].sequence, 0, GB);
+                 if (option == "r")
+                       r.NextBytes(arrayofbytes[i].sequence);
+                 else if (option =="f")
+                    for (int n = 0; n < GB; n++)
+                        arrayofbytes[i].sequence[n] = 255;
+                 else
+                    Array.Clear(arrayofbytes[i].sequence, 0, GB);
              });
+
+            //  fill the remaining bytes
+            if (option == "r")
+                r.NextBytes(lastbytes);
+            else if (option == "f")
+                for (uint n = 0; n < remaining; n++)
+                    lastbytes[n] = 255;
+            else
+                Array.Clear(lastbytes, 0, (int) remaining);
 
             DateTime tend = DateTime.Now;
             TimeSpan t = tend.Subtract(tstart);
             Console.Write("     time: " + t.TotalSeconds.ToString("0.000") + " seconds  (" + (  GBs/t.TotalSeconds).ToString("0.0") + " GB/s)\n");
 
 
-            //       long totalsize = size * n;
-            Console.WriteLine(GBs + " GB Filled");
+            Console.WriteLine(totalbytes + " GB Filled with " + filler);
 
-            //System.Threading.Thread.Sleep(30000);
-
-            /*        for (int i = 0; i < size; i++)
-                     {
-                         int x = r.Next(0, size);
-                         int y = r.Next(0, n);
-                         byte bytexy = bufptr[y][x];
-                     }
-
-
-
-
-                     // System.Threading.Thread.Sleep(10000);
-                 }
-
-                 private static void Randomfill(int j, int k)
-                 {
-                     // need to make this faster
-                     r.NextBytes(bufptr[j,k]);
-                     for (int i = 0; i < j; i++)
-                         for (int n = 0; n < k; n++)
-                             bufptr[i,n] = (byte) r.Next(0, 255);
-                 }
-             */
 
         }
     }
