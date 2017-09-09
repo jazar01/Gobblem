9566cdde3435f3bb6c621cc36d1e5d628a31a01a
jazar01 <jim.azar@orasi.com>
2017-09-09 11:28:34 -0400

    Refactored and added -a option

--- Program.cs
+++ Program.cs
@@ -12,75 +12,89 @@ namespace Gobble
 {
     class Program
     {
-    static string option;
-     struct bytes
-        {
-            public byte[] sequence;
-        }
 
+    struct bytes
+    {
+        public byte[] sequence;
+    }
+
+    const int MB = 1024 * 1024;
+    const int GB = MB * 1024;
 
         static void Main(string[] args)
         {
-            int MB = 1024 * 1024;
-            int Unit = MB * 1024;
-            int GB = MB * 1024;
+            string parm;
+            if (args.Length == 0)
+                parm = "-c";
+            else
+                parm = args[0];
 
-            RNGCryptoServiceProvider CSP = new RNGCryptoServiceProvider();
-            ComputerInfo CI = new ComputerInfo();
+            if (parm == "-a")
+            {
+                fillmem("-c");
+                fillmem("-f");
+                fillmem("-r");
+            }
+            else
+                fillmem(parm);
+        }
 
+
+        static void fillmem (string parm)
+            {
+            int Unit;
+            string option;
             string filler;
             int maxthreads;
 
-            
-            filler = "0's";
-            maxthreads = 2;
-            if (args.Length == 0)
-                option = "c";
-            else
-                switch (args[0])
+            switch (parm)
                 {
-                    case "-r":
-                        option = "r";
-                        filler = "random bytes";
-                        maxthreads = 1;   // CSP chokes on multiple threads
-                        Unit = MB * 256;                 
-                        break;
-                    case "-f":
-                    case "-1":
-                        option = "f";
-                        filler = "1's";
-                        maxthreads = 12;
-                        Unit = MB * 1024;
-                        break;
-                    default:
-                        option = "c";
-                        maxthreads = 2;
-                        Unit = MB * 1024;
-                        break;
+                case "-r":
+                    option = "r";
+                    filler = "random bytes";
+                    maxthreads = 1;   // CSP chokes on multiple threads
+                    Unit = MB * 256;                 
+                    break;
+                case "-f":
+                case "-1":
+                    option = "f";
+                    filler = "1's";
+                    maxthreads = 12;
+                    Unit = MB * 1024;
+                    break;
+                case "-c":
+                case "-0":
+                default:
+                    option = "c";
+                    filler = "0's";
+                    maxthreads = 2;
+                    Unit = MB * 1024;
+                    break;
                 }
 
-
-            
-            ulong totalMem = CI.TotalPhysicalMemory;
+            // get info about available memory
+            ComputerInfo CI = new ComputerInfo();
             ulong availablePMem = CI.AvailablePhysicalMemory;
-
-            int TotalGBs = (int)((uint)totalMem / Unit);
-
             int Units = (int) (availablePMem / (ulong) Unit);
-            int size = TotalGBs;
+
             bytes[] arrayofbytes;
             DateTime tstart = DateTime.Now;
+
+            // this causes the memory to become committed to the process
             arrayofbytes = new bytes[Units];
             for (int i = 0; i < Units; i++)
                 arrayofbytes[i].sequence = new byte[Unit];
 
-            //todo allocate last bytes of memory here
+            //commit last bytes of memory here
             ulong remaining = availablePMem - ((ulong) Units * (ulong) Unit );
             byte[] lastbytes = new byte[remaining] ;
 
             double factor = (double) Unit / (double) GB;
             string totalbytes = ((double)Units * factor + (double)lastbytes.Length / (double)GB).ToString("0.00");
             Console.WriteLine(totalbytes + " committed\n");
+
+
+            RNGCryptoServiceProvider CSP = new RNGCryptoServiceProvider();
             if (option == "r")
                 for (int i = 0; i < Units; i++)
                 {
@@ -111,12 +125,16 @@ namespace Gobble
                 Array.Clear(lastbytes, 0, (int) remaining);
 
             CSP.Dispose();
+            arrayofbytes = null;
+            lastbytes = null;
 
             DateTime tend = DateTime.Now;
             TimeSpan t = tend.Subtract(tstart);
             Console.Write("\n" + totalbytes + " GB Filled with " + filler);
-            Console.Write("  in " + t.TotalSeconds.ToString("0.00") + " seconds  (" + (  Units/t.TotalSeconds).ToString("0.00") + " GB/s)");
-      
+            Console.WriteLine("  in " + t.TotalSeconds.ToString("0.00") + " seconds  (" + (  Units/t.TotalSeconds).ToString("0.00") + " GB/s)");
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
 
         }
     }
